754cc2c4a13b8938572fc7ac5c3c3355af42649b
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-03-15 12:31:18 +0530

    Added constants for user roles, order statuses, payment providers, and coupon types

--- backend/src/constants.js
+++ backend/src/constants.js
@@ -1,1 +1,30 @@
 export const DB_NAME = "Furnit";
+
+export const UserRoles = {
+  ADMIN: "ADMIN",
+  USER: "USER",
+};
+
+export const AvailableUserRoles = Object.values(UserRoles);
+
+export const OrderStatus = {
+  PENDING: "PENDING",
+  CANCELLED: "CANCELLED",
+  DELIVERED: "DELIVERED",
+};
+
+export const AvailableOrderStatus = Object.values(OrderStatus);
+
+// add payment provider service
+export const PaymentProvider = {
+  UNKNOWN: "UNKNOWN",
+};
+
+export const AvailablePaymentProvider = Object.values(PaymentProvider);
+
+export const CouponTypes = {
+  FLAT: "FLAT",
+  PERCENTAGE: "PERCENTAGE",
+};
+
+export const AvailableCouponType = Object.values(CouponTypes);
