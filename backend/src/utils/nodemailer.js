ace0d2eea0cadbad830289794cea03b23ae6eda4
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-04-01 12:28:49 +0530

    config nodemailer to send email

---
+++ backend/src/utils/nodemailer.js
@@ -0,0 +1,26 @@
+import nodemailer from "nodemailer";
+
+const sendEmail = async (email, subject, link) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.HOST,
+      port: process.env.PORT,
+      secure: true,
+      auth: {
+        user: process.env.USER_EMAIL,
+        pass: process.env.USER_PASSWORD,
+      },
+    });
+
+    await transporter.sendMail({
+      from: process.env.USER_EMAIL,
+      to: email,
+      subject: subject,
+      text: link,
+    });
+  } catch (error) {
+    console.log("Email not send");
+  }
+};
+
+export { sendEmail };
