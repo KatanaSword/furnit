0f6dfa423e8a9bcbc2887a2608401aad8fc507e7
KatanaSword <114555620+KatanaSword@users.noreply.github.com>
2024-04-16 13:38:44 +0530

    Adding style on header component

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,10 +1,10 @@
-import { Link } from "react-router-dom";
-import { Img, Text, Input } from "../../components";
+import { Link, NavLink } from "react-router-dom";
+import { Img, Text } from "../../components";
 
-const Header = (props) => {
+const Header = ({ className }) => {
   return (
     <>
-      <header className={props.className}>
+      <header className={className}>
         <div className="flex md:flex-col flex-row md:gap-10 items-center justify-between w-full">
           <div className="header-row ">
             <Link to="/">
@@ -21,46 +21,56 @@ const Header = (props) => {
             </div>
           </div>
           <div className="flex sm:flex-1 flex-row gap-9 sm:hidden items-center justify-between w-[498px] sm:w-full">
-            <Link to="/shop">
-              <Text
-                className="text-black-900 text-lg tracking-[-0.50px] w-auto"
-                size="txtRubikRomanRegular18"
-              >
-                Shop
-              </Text>
-            </Link>
-            <Link to="/wishlist">
-              <Text
-                className="text-black-900 text-lg tracking-[-0.50px] w-auto"
-                size="txtRubikRomanRegular18"
-              >
-                Wishlist
-              </Text>
-            </Link>
-            <Link to="/aboutus">
-              <Text
-                className="text-black-900 text-lg tracking-[-0.50px] w-auto"
-                size="txtRubikRomanRegular18"
-              >
-                About
-              </Text>
-            </Link>
-            <Link to="/contactus">
-              <Text
-                className="text-black-900 text-lg tracking-[-0.50px] w-auto"
-                size="txtRubikRomanRegular18"
-              >
-                Contact
-              </Text>
-            </Link>
-            <Link to="/team">
-              <Text
-                className="text-black-900 text-lg tracking-[-0.50px] w-auto"
-                size="txtRubikRomanRegular18"
-              >
-                Team
-              </Text>
-            </Link>
+            <NavLink
+              to="/shop"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-reddish_orange-S700 text-lg tracking-[-0.50px] w-auto"
+                  : "text-black-900 text-lg tracking-[-0.50px] w-auto"
+              }
+            >
+              <Text size="txtRubikRomanRegular18">Shop</Text>
+            </NavLink>
+            <NavLink
+              to="/wishlist"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-reddish_orange-S700 text-lg tracking-[-0.50px] w-auto"
+                  : "text-black-900 text-lg tracking-[-0.50px] w-auto"
+              }
+            >
+              <Text size="txtRubikRomanRegular18">Wishlist</Text>
+            </NavLink>
+            <NavLink
+              to="/aboutus"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-reddish_orange-S700 text-lg tracking-[-0.50px] w-auto"
+                  : "text-black-900 text-lg tracking-[-0.50px] w-auto"
+              }
+            >
+              <Text size="txtRubikRomanRegular18">About</Text>
+            </NavLink>
+            <NavLink
+              to="/contactus"
+              className={({ isActive }) =>
+                isActive
+                  ? " text-reddish_orange-S700 text-lg tracking-[-0.50px] w-auto"
+                  : "text-black-900 text-lg tracking-[-0.50px] w-auto"
+              }
+            >
+              <Text size="txtRubikRomanRegular18">Contact</Text>
+            </NavLink>
+            <NavLink
+              to="/team"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-reddish_orange-S700 text-lg tracking-[-0.50px] w-auto"
+                  : "text-black-900 text-lg tracking-[-0.50px] w-auto"
+              }
+            >
+              <Text size="txtRubikRomanRegular18">Team</Text>
+            </NavLink>
           </div>
           <div className="flex sm:hidden justify-center items-center gap-8">
             <Link to="/signup">
@@ -70,20 +80,25 @@ const Header = (props) => {
                 alt="icon"
               />
             </Link>
-            <Link to="/cart">
+            <NavLink
+              to="/cart"
+              className={({ isActive }) =>
+                isActive
+                  ? "bg-yellow-100 text-lg tracking-[-0.50px] w-auto p-2 rounded-full"
+                  : "text-black-900 text-lg tracking-[-0.50px] w-auto p-2"
+              }
+            >
               <Img
                 className="h-6 w-6 object-cover object-right"
                 src="images/img_icon.svg"
                 alt="icon"
               />
-            </Link>
+            </NavLink>
           </div>
         </div>
       </header>
     </>
   );
 };
-
-Header.defaultProps = {};
 
 export default Header;
