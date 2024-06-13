747607356e671803db084e502bc8abfc4bc355b7
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-06-13 23:37:40 +0530

    Remove file functions

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -1,20 +1,3 @@
-import fs from "fs";
-
-export const getStaticFilePath = (req, fileName) => {
-  return `${req.protocol}://${req.get("host")}/images/${fileName}`;
-};
-
-export const getLocalPath = (fileName) => {
-  return `public/images${fileName}`;
-};
-
-export const removeLocalFile = (localPath) => {
-  fs.unlink(localPath, (err) => {
-    if (err) console.log("Something went wrong while removing image:", err);
-    console.log("Image remove successfully", localPath);
-  });
-};
-
 export const getMongoosePaginationOptions = ({
   page = 1,
   limit = 10,
