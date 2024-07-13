3f1a7da34461c4cdb148cdfa1f3eab483c41aa43
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-07-13 13:43:03 +0530

    Declear order router

--- backend/src/app.js
+++ backend/src/app.js
@@ -26,6 +26,7 @@ import couponRouter from "./routers/coupon.routers.js";
 import wishlistRouter from "./routers/wishlist.routers.js";
 import contactusRouter from "./routers/contactus.routers.js";
 import cartRouter from "./routers/cart.routers.js";
+import orderRouter from "./routers/order.routers.js";
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
@@ -37,5 +38,6 @@ app.use("/api/v1/coupons", couponRouter);
 app.use("/api/v1/wishlists", wishlistRouter);
 app.use("/api/v1/contactus", contactusRouter);
 app.use("/api/v1/carts", cartRouter);
+app.use("/api/v1/orders", orderRouter);
 
 export { app };
