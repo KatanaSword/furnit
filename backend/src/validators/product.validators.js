0c51d8c9ed18f100b0803268561875b18f0792b0
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-07-19 16:45:02 +0530

    Add productUpdateRequiredBodyValidator

--- backend/src/validators/product.validators.js
+++ backend/src/validators/product.validators.js
@@ -31,4 +31,40 @@ const productCreateRequiredBodyValidator = () => {
   ];
 };
 
-export { productCreateRequiredBodyValidator };
+const productUpdateRequiredBodyValidator = () => {
+  return [
+    body("name")
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage("Product name is required")
+      .isLength({ min: 4 })
+      .withMessage("Product name must be at lease 4 characters long"),
+    body("description")
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage("Description is required"),
+    body("category").trim().notEmpty().withMessage("Category is required"),
+    body("price")
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage("Product price is required")
+      .isNumeric()
+      .withMessage("Price must be a number"),
+    body("stock")
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage("Stock is required")
+      .isNumeric()
+      .withMessage("Stock must be a number"),
+    ...mongoIdRequestBodyValidator("category"),
+  ];
+};
+
+export {
+  productCreateRequiredBodyValidator,
+  productUpdateRequiredBodyValidator,
+};
