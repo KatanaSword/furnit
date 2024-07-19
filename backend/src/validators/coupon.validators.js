8d0192f254f4171cb202f492194f91b813f14b3f
Saurabh Tajane <114555620+KatanaSword@users.noreply.github.com>
2024-07-19 15:32:42 +0530

    Add couponUpdateRequestBodyValidator

--- backend/src/validators/coupon.validators.js
+++ backend/src/validators/coupon.validators.js
@@ -1,5 +1,5 @@
 import { body } from "express-validator";
-import { AvailableCouponType } from "../constants.js";
+import { AvailableCouponType, options } from "../constants.js";
 
 const couponCreateRequestBodyValidator = () => {
   return [
@@ -46,4 +46,51 @@ const couponCreateRequestBodyValidator = () => {
   ];
 };
 
-export { couponCreateRequestBodyValidator };
+const couponUpdateRequestBodyValidator = () => {
+  return [
+    body("name").options().trim().notEmpty().withMessage("Name is required"),
+    body("couponCode")
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage("Coupon code is required")
+      .isLength({ min: 4 })
+      .withMessage("Coupon code must be at lease 4 characters long"),
+    body("type")
+      .optional()
+      .isIn(AvailableCouponType)
+      .withMessage("Invalid coupon type"),
+    body("discountValue")
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage("Discount value is required")
+      .isInt({ min: 1 })
+      .withMessage("Discount value must be greater than 0"),
+    body("minimumCartValue")
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage("Invalid minimum cart value")
+      .isInt({ min: 0 })
+      .withMessage("Minimum cart value cannot be negative"),
+    body("startDate")
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage("Start date is required")
+      .isISO8601()
+      .withMessage("Invalid start date. Date must be in ISO8601 format"),
+    body("expiryDate")
+      .optional()
+      .trim()
+      .notEmpty()
+      .withMessage("Expiry date is required")
+      .isISO8601()
+      .withMessage("Invalid expiry date. Date must be in ISO8601 format")
+      .isAfter(new Date().toISOString())
+      .withMessage("Expiry date must be a future date"),
+  ];
+};
+
+export { couponCreateRequestBodyValidator, couponUpdateRequestBodyValidator };
